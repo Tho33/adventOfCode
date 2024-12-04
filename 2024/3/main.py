898cdea5679b62244dda2c7834d363d1f1a536fa
Tho33 <theo.gregorio@laposte.net>
2024-12-04 21:58:28 +0100

    day 3 part 2

--- 2024/3/main.py
+++ 2024/3/main.py
@@ -1,6 +1,15 @@
 import re
-def getMultiplyList(line:str) -> list:
-    return re.findall("mul\\([0-9]{1,3},[0-9]{1,3}\\)",line)
+def isMultiplyInstruction(line:str) -> bool:
+    return re.search("mul\\([0-9]{1,3},[0-9]{1,3}\\)", line) != None
+
+def isDoInstruction(line:str) -> bool:
+    return re.search("do\\(\\)", line) != None
+
+def isDontInstruction(line:str) -> bool:
+    return re.search("don\\'t\\(\\)", line) != None
+
+def getMultiplyListWithSwitches(line:str) -> list:
+    return re.findall("mul\\([0-9]{1,3},[0-9]{1,3}\\)|do\\(\\)|don\\'t\\(\\)",line)
 
 def getMultiplyResult(list) -> int:
     return int(list[0]) * int(list[1])
@@ -12,13 +21,21 @@ def main():
 
     multiplyList=[]
     sumResult=0
+    doSwitch=1
+
 
     with open("input.txt", "r") as inputFile:
         for line in inputFile:
-            multiplyList = getMultiplyList(line)
+            multiplyList = getMultiplyListWithSwitches(line)
             for elem in multiplyList:
-                sumResult += getMultiplyResult(getMultiplyFactors(elem))
-    print (sumResult)
+                if(isDoInstruction(elem)):
+                    doSwitch = 1
+                if(isDontInstruction(elem)):
+                    doSwitch = 0
+                if(isMultiplyInstruction(elem)):
+                    sumResult += doSwitch * getMultiplyResult(getMultiplyFactors(elem))
+
+    print ("Result : ", sumResult)
 
     return 0
 
