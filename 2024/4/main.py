09ccb32b5992efb8b0ae662c7070336e02faafab
Tho33 <theo.gregorio@laposte.net>
2024-12-07 12:21:32 +0100

    Day 4 part 1 OK

--- 2024/4/main.py
+++ 2024/4/main.py
@@ -36,17 +36,31 @@ def getCombinationsMoves() -> list:
 
 def getPossibleCombinationsMoves(arrayWidth:int, arrayHeight:int, startingPosition:list) -> list :
     wordSize=3 #XMAS is 4 letters, but we take in account the 0 in array
-    startingX=startingPosition[0]
-    startingY=startingPosition[1]
+    startingY=startingPosition[0]
+    startingX=startingPosition[1]
     moves=[]
-    if (startingX - wordSize)>=0:
+    if ((startingX - wordSize)>=0):
         moves.append(getCoordDeltaForHorizontalLeft())
-    if (startingX + wordSize)<arrayWidth:
+    if ((startingX + wordSize)<arrayWidth):
         moves.append(getCoordDeltaForHorizontalRight())
-    if (startingY - wordSize)>=0:
+    if ((startingY - wordSize)>=0):
         moves.append(getCoordDeltaForVerticalUp())
-    if (startingY + wordSize)<arrayHeight:
+    if ((startingY + wordSize)<arrayHeight):
         moves.append(getCoordDeltaForVerticalDown())
+
+    if (((startingX - wordSize)>=0) & ((startingY + wordSize)<arrayHeight)):
+        print("downLeft")
+        moves.append(getCoordDeltaForDiagonalDownLeft())
+    if (((startingX + wordSize)<arrayWidth) & ((startingY + wordSize)<arrayHeight)):
+        print("downRight")
+        moves.append(getCoordDeltaForDiagonalDownRight())
+    if (((startingX - wordSize)>=0) & ((startingY - wordSize)>=0)):
+        print("UpLeft")
+        moves.append(getCoordDeltaForDiagonalUpLeft())
+    if (((startingX + wordSize)<arrayWidth) & ((startingY - wordSize)>=0)):
+        print("UpRight")
+        moves.append(getCoordDeltaForDiagonalUpRight())
+    
 
     return moves
 
@@ -56,6 +70,8 @@ def containsWordWithPattern(doubleArray:list, startingPosition:list, getCoordDel
         isALetter = doubleArray[startingPosition[0] + getCoordDeltaToCheck[2][0]][startingPosition[1] + getCoordDeltaToCheck[2][1]] == 'A'
         isSLetter = doubleArray[startingPosition[0] + getCoordDeltaToCheck[3][0]][startingPosition[1] + getCoordDeltaToCheck[3][1]] == 'S'
         
+        if(isXLetter & isMLetter & isALetter & isSLetter):
+            print ("XMAS at : ", startingPosition, "with ", getCoordDeltaToCheck)
         return isXLetter & isMLetter & isALetter & isSLetter
 
 def main():
@@ -66,7 +82,8 @@ def main():
     with open("input.txt", "r") as inputFile:
         for line in inputFile:
             listedLine=list(line)
-            listedLine.pop(len(listedLine) -1)
+            if(listedLine[len(listedLine) -1] == '\n'):
+                listedLine.pop(len(listedLine) -1)
             inputArray.append(listedLine)
 
     print (inputArray)
@@ -77,12 +94,10 @@ def main():
                 startingPosition=[lineCount, columnCount]
                 combinations = getPossibleCombinationsMoves(len(inputArray),len(inputArray[0]), startingPosition)
                 for direction in combinations:
-                    print ("direction : ", direction)
-                    print ("start : ", startingPosition)
                     if (containsWordWithPattern(inputArray, startingPosition, direction)):
                         count+=1
 
-
+    print (count)
     return 0
 
 if __name__ == '__main__':
